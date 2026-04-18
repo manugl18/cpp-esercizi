18a2ab019bfaaba6ccd3dba9e5376157b010b961
Manuel <manuelsanzo18@gmail.com>
2026-04-18 18:10:45 +0200

    fatto es 6

---
+++ Laboratorio5/Laboratorio5/es6.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "Lqueue.h"
+#include "LQueueTest.h"
+
+using namespace std;
+
+int main() {
+
+	int n_numeri = 0;
+	LQueue<Item> coda;
+	LQueue<Item> coda2;
+	float n = 0;
+	cout << "quanti numeri vuoi inserire? \n";
+	cin >> n_numeri;
+	cout << "Inserisci i numeri float: \n";
+
+	for (int i = 0; i < n_numeri; i++) {
+		cin >> n;
+		coda.enqueue(Item(n));
+	}
+	// RICORDA BENE: Nelle CODE, l'ultimo elemento che inseriamo va IN FONDO ALLA CODA.
+	int cnt = 0;
+	while (coda.length() > 1) {
+		int l = coda.length();
+		for (int i = 0; i < l / 2; i++) {
+			Item it1 = coda.dequeue();
+			Item it2 = coda.dequeue();
+
+			float valore = 0;
+			switch (cnt % 4)
+			{
+			case(0): valore = it1.key() + it2.key();
+				break;
+			case(1): valore = it1.key() - it2.key();
+				break;
+			case(2): valore = it1.key() * it2.key();
+				break;
+			case(3): valore = it1.key() / it2.key();
+				break;
+
+			}
+			coda.enqueue(Item(valore));
+		}
+		if (l % 2 != 0) {
+			Item it1 = coda.dequeue();
+			coda.enqueue(it1);
+		}
+		cnt++;
+		cout << "Q1 is: ";
+		Lqueueprint(coda);
+		cout << "\n";
+	}
+	return 0;
+}
